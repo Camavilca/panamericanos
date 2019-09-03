07af9fa0bcfc67d40f96edefe021de25b7440e33
Camavilca <orlando.camavilca@tecsup.edu.pe>
2019-09-02 21:58:05 -0600

    fix

---
+++ src/main/java/com/panamericano/PanamericanosApplication.java
@@ -0,0 +1,13 @@
+package com.panamericano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanamericanosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PanamericanosApplication.class, args);
+	}
+
+}
